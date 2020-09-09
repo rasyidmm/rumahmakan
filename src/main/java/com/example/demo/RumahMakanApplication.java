4bac9920e0f3eb6321cf3190317d37da6087ac2a
rasyid maulid <rasyid.maulid@lawencon.com>
2020-09-09 20:58:28 +0700

    fixing swager

--- src/main/java/com/example/demo/RumahMakanApplication.java
+++ src/main/java/com/example/demo/RumahMakanApplication.java
@@ -12,11 +12,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
-@EnableSwagger2WebMvc
+
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class RumahMakanApplication {
 
 	public static void main(String[] args) {
@@ -26,29 +28,22 @@ public class RumahMakanApplication {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controlle"))
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(apiDetails());
     }
-//	@Bean
-//    public Docket swaggerConfiguration() {
-//
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.demo.Controller"))
-//                .build()
-//                .apiInfo(apiDetails());
-//    }
-//	private ApiInfo apiDetails() {
-//        return new ApiInfo(
-//                "Apps RumahMakan",
-//                "Api Documentation",
-//                "1.0",
-//                "",
-//                new springfox.documentation.service.Contact("Rasyid", "", "rasyid"),
-//                "",
-//                "",
-//                Collections.emptyList());
-//    }
+	
+	private ApiInfo apiDetails() {
+      return new ApiInfo(
+              "Apps RumahMakan",
+              "Api Documentation",
+              "1.0",
+              "",
+              new springfox.documentation.service.Contact("Rasyid", "", "rasyidmaulidmajid@gmail.com"),
+              "",
+              "",
+              Collections.emptyList());
+  }
+
 
 }
